4562bd1f10b947a70dee8b82705d4e5260a43fd5
Breno Araújo <brenod123@gmail.com>
2025-10-22 14:05:47 -0300

    att

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
+DEBUG = True
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG)
