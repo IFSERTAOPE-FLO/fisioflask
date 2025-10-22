9e519925fb6c905a8b7f46106871c0f1aa99ff25
Breno Araújo <brenod123@gmail.com>
2025-10-22 13:46:56 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template
-
-DEBUG = True
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=DEBUG)
+def hello_world():
+    return 'Hello, World!'
